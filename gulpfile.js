f8a8e6c3d44095ebb2a27aa9fe71a882d3c721a3
Jose Perez <j.perez@adinspector.pe>
2017-06-21 18:45:29 -0500

    Maqueta Blog

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('inject', function () {
 gulp.task('uncss', ['compress'], function () {
   return gulp.src('./wp-content/themes/cbb/css/style.min.css')
             .pipe(uncss({
-              html: ['./app/index.html', './app/nosotros.html', './app/propuesta.html']
+              html: ['./app/index.html', './app/nosotros.html', './app/propuesta.html', './app/blog.html']
             }))
             .pipe(gulp.dest('./wp-content/themes/cbb/css'));
 });
