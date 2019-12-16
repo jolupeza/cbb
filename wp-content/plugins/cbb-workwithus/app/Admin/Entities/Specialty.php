94a1af10f11cff6db0de772f3a186c98f1b1579e
Jose Perez <jose.perez@altimea.com>
2019-12-16 10:32:06 -0500

    [WorkWithUs] Unregister post type specialties

--- wp-content/plugins/cbb-workwithus/app/Admin/Entities/Specialty.php
+++ wp-content/plugins/cbb-workwithus/app/Admin/Entities/Specialty.php
@@ -27,56 +27,12 @@ class Specialty
 
     public function init()
     {
-        $this->loader->add_action('init', $this, 'registerPostType');
+        $this->loader->add_action('init', $this, 'unRegisterPostType');
     }
 
-    public function registerPostType()
+    public function unRegisterPostType()
     {
-        $labels = array(
-            'name' => __('Especialidades', $this->domain),
-            'singular_name' => __('Especialidad', $this->domain),
-            'add_new' => __('Nueva especialidad', $this->domain),
-            'add_new_item' => __('Agregar nueva especialidad', $this->domain),
-            'edit_item' => __('Editar especialidad', $this->domain),
-            'new_item' => __('Nueva especialidad', $this->domain),
-            'view_item' => __('Ver especialidad', $this->domain),
-            'search_items' => __('Buscar especialidad', $this->domain),
-            'not_found' => __('Especialidad no encontrada', $this->domain),
-            'not_found_in_trash' => __('Especialidad no encontrada en la papelera', $this->domain),
-            'all_items' => __('Todos las especialidades', $this->domain),
-        );
-        $args = array(
-            'labels' => $labels,
-            'description' => 'Listado de Especialidades.',
-            // 'public'              => false,
-            // 'exclude_from_search' => true,
-            // 'publicly_queryable' => false,
-            'show_ui' => true,
-            'show_in_nav_menus' => false,
-            'show_in_menu' => true,
-            'show_in_admin_bar' => true,
-            'show_in_rest' => true,
-            // 'menu_position'          => null,
-            'menu_icon' => 'dashicons-welcome-learn-more',
-            // 'hierarchical'        => false,
-            'supports' => array(
-                'title',
-                // 'editor',
-//                'custom-fields',
-                'author',
-                // 'thumbnail',
-                'page-attributes',
-                // 'excerpt'
-                // 'trackbacks'
-                // 'comments',
-                // 'revisions',
-                // 'post-formats'
-            ),
-            // 'taxonomies'  => array('post_tag', 'category'),
-            // 'has_archive' => false,
-            'rewrite' => false
-        );
-        register_post_type('specialties', $args);
+        unregister_post_type('specialties');
     }
 
 }
